392d66937abb0c0e8084d80a7ea564375b211a2c
isael94 <javi@MacBook-Pro-de-Javier.local>
2020-07-14 12:41:38 -0500

    Adding GET an POST

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://javisael:d12world@kodemia-node-live.av6ij.mongodb.net/school?retryWrites=true&w=majority', 
-    {   useNewUrlParser: true, 
-        useUnifiedTopology: true
-    }, () => {
-        console.log('DB Connected...')
-        Student.create({
-            name: 'Javier',
-            course: 'Ingles',
-            age: 26
-        })
-    });
+const express = require('express');
+const { response } = require('express');
+
+const app = express()
 
 const Student = mongoose.model('Students', {
     name: {
@@ -30,4 +23,51 @@ const Student = mongoose.model('Students', {
         min: 1
     }
 })
+app.use(express.json())
+
+app.get('/students', async (request, response) => {
+    try{
+    const allStudents = await Student.find()
+
+    response.json({
+        success: true,
+        data: {
+            students: allStudents
+        }
+    })
+    }catch(error){
+        response.status(400)
+        response.json({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+app.post('/students', async (request, response) =>{
+    const studentInfo = request.body
+
+    const newStudent = await Student.create(studentInfo)
+
+    response.json({
+        success: true,
+        data: {
+            student: newStudent
+        }
+    })
+})
+
+
+
+mongoose.connect('mongodb+srv://javisael:d12world@kodemia-node-live.av6ij.mongodb.net/school?retryWrites=true&w=majority', 
+    {   useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, () => {
+        console.log('DB Connected...')
+        app.listen(8080, () =>{
+            console.log('Server is ready...')
+        })
+    })
+   
+
 
