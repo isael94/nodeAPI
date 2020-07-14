5eabab8583b837ecf725f5867ebc6d0259971071
isael94 <javi@MacBook-Pro-de-Javier.local>
2020-07-14 12:01:55 -0500

    Mongoose Connected

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log("Hola coders!")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://javisael:d12world@kodemia-node-live.av6ij.mongodb.net/school?retryWrites=true&w=majority', 
+    {   useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, () => {
+        console.log('DB Connected...')
+        Student.create({
+            name: 'Javier',
+            course: 'Ingles',
+            age: 26
+        })
+    });
+
+const Student = mongoose.model('Students', {
+    name: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true,
+        enum: [
+            'Programacion',
+            'Ingles',
+            'Cocina'
+        ]
+    },
+    age: {
+        type: Number,
+        min: 1
+    }
+})
 
